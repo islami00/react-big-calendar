1819224960c6e229cd00e0ad8cf2318dc710aa41
Islam <islamkwnd@gmail.com>
2024-10-01 12:04:50 +0100

    revert: remove dist and lib, ensuring src-main is clean for merging

--- lib/components.types.ts
+++
@@ -1,7 +0,0 @@
-import type { RBCResource } from './misc.types'
-
-export interface TimeSlotWrapperProps<TResource extends object = RBCResource> {
-  children: React.ReactNode
-  value: Date
-  resource: null | TResource
-}
